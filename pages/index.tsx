be009601e9ee8733a845c8477680c394488a9228
Exoazul <exorcistazul@gmail.com>
2024-10-15 23:00:59 -0600

    practica2martes

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+interface DemoProps {}
+
+export default function Demo({}: DemoProps) {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('The count is:', count);
+
+    return () => {
+      console.log('I am being cleaned up!');
+    };
+  }, [count]);
+
+  return (
+    <div className='tutorial'>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+
